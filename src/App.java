1127ccca468829a534df471c40d996b66c87b7fe
unknown <gabrielreis.reis1903@gmail.com>
2023-08-14 12:08:40 -0300

    Utilitario adicionado

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ public class App {
         System.out.println("Teste");
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA,6,45);
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL,8,70);
+
 
         //Abastecer.
         System.out.println("\nAbastecendo o carro");
@@ -12,6 +14,8 @@ public class App {
         System.out.println(basico);
         esportivo.abastece(TipoCombustivel.GASOLINA,45);
         System.out.println(esportivo);
+        utilitario.abastece(TipoCombustivel.DIESEL,70);
+        System.out.println(utilitario);
 
         //Distancia da viagem.
         System.out.println("\nViajando");
@@ -19,6 +23,8 @@ public class App {
         System.out.println(basico);
         esportivo.viaja(100);
         System.out.println(esportivo);
+        utilitario.viaja(100);
+        System.out.println(utilitario);
 
     }
 }
