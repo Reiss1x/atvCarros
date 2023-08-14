a012ac75a14572d532e90271f3fa9003569a6c87
unknown <gabrielreis.reis1903@gmail.com>
2023-08-14 12:07:18 -0300

    Esportivo adicionado

--- src/App.java
+++ src/App.java
@@ -4,17 +4,21 @@ public class App {
 
         System.out.println("Teste");
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA,6,45);
 
-        System.out.println("Tipos de veiculos:");
-        System.out.println(basico);
-
-        System.out.println("\n\n----------------");
-        System.out.println("\nAbastencendo carro basico com gasolina");
+        //Abastecer.
+        System.out.println("\nAbastecendo o carro");
         basico.abastece(TipoCombustivel.GASOLINA, 55);
         System.out.println(basico);
-        System.out.println("\nViajando com o carro basico");
-        basico.viaja(250);
-        basico.viaja(150);
+        esportivo.abastece(TipoCombustivel.GASOLINA,45);
+        System.out.println(esportivo);
+
+        //Distancia da viagem.
+        System.out.println("\nViajando");
+        basico.viaja(100);
         System.out.println(basico);
+        esportivo.viaja(100);
+        System.out.println(esportivo);
+
     }
 }
